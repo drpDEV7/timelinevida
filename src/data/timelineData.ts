cb96957df42e09ec757aec3081ea10490a1777ab
drpDEV7 <ds.47840337856@gmail.com>
2025-05-23 07:15:38 -0300

    Movendo imagens para pasta public

--- src/data/timelineData.ts
+++ src/data/timelineData.ts
@@ -12,7 +12,7 @@ export const timelineData: TimelineItemData[] = [
     year: '2008',
     title: 'O Começo',
     description: 'Nasci em 26 de fevereiro de 2008. O início da minha jornada neste mundo, com possibilidades infinitas pela frente. Mal sabia eu sobre a revolução tecnológica que eu testemunharia e da qual me tornaria parte.',
-    image: '/img/IMG/FOTO PERFIL.png',
+    image: '/images/FOTO PERFIL.png',
     animationEffect: 'fade-right',
     technologies: ['Vida', 'Novos Começos']
   },
@@ -20,7 +20,7 @@ export const timelineData: TimelineItemData[] = [
     year: '2014',
     title: 'Entrada no SESI',
     description: 'Ingressei na escola SESI, marcando o início da minha jornada na educação formal. Este foi um passo crucial que estabeleceria a base para minhas futuras conquistas acadêmicas e interesse em tecnologia.',
-    image: '/img/IMG/sesi-logo.png',
+    image: '/images/sesi-logo.png',
     animationEffect: 'fade-left',
     technologies: ['Educação', 'Aprendizado', 'Crescimento']
   },
@@ -28,7 +28,7 @@ export const timelineData: TimelineItemData[] = [
     year: '2015',
     title: 'Primeira Experiência com Computador',
     description: 'Minha primeira interação com um computador, abrindo as portas para um mundo digital cheio de maravilhas. Este momento despertou minha curiosidade sobre como essas máquinas funcionam e o que elas poderiam fazer.',
-    image: '/img/IMG/notebook sem fundo.png',
+    image: '/images/notebook sem fundo.png',
     animationEffect: 'zoom-in',
     technologies: ['Computador', 'Windows', 'Mundo Digital']
   },
@@ -36,7 +36,7 @@ export const timelineData: TimelineItemData[] = [
     year: '2016',
     title: 'Descoberta dos Games',
     description: 'Primeiro contato com jogos online como Rocket League. Os games me apresentaram a comunidades virtuais e às possibilidades empolgantes de experiências digitais interativas. Foi quando comecei a apreciar a criatividade e habilidade técnica por trás do desenvolvimento de jogos.',
-    image: '/img/IMG/ROCKET LEAGUE.png',
+    image: '/images/ROCKET LEAGUE.png',
     animationEffect: 'flip-up',
     technologies: ['Rocket League', 'Jogos Online', 'Multiplayer']
   },
@@ -52,7 +52,7 @@ export const timelineData: TimelineItemData[] = [
     year: '2024',
     title: 'Início da Jornada no SENAI SP',
     description: 'Me matriculei no SENAI SP para estudar formalmente tecnologia e programação. Levando minha paixão ao nível profissional com educação estruturada em desenvolvimento de software e inovação digital. Isso marca o início da minha carreira em tecnologia.',
-    image: '/img/IMG/LOGO SENAI.jpg',
+    image: '/images/LOGO SENAI.jpg',
     animationEffect: 'fade-up-left',
     technologies: ['Educação Profissional', 'Desenvolvimento de Software', 'Carreira']
   }
